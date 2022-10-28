c9db30a13aeb1caaeab7746f9af5554bd96c4905
Erin Ponsonby <erinponsonby@gmail.com>
2022-10-28 09:01:04 -0600

    Add code to repo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,86 @@
 import logo from './logo.svg';
+import { useState} from "react"
 import './App.css';
+import validator from "validator"
 
 function App() {
+
+  const [signupInput, setSignupInput] = useState({
+    email: "",
+    password: "",
+    confirmPassword: ""
+  })
+
+  const [error, setError] = useState("")
+
+  const handleChange = (e) => {
+      setSignupInput({
+        ...signupInput,
+        [e.target.name]: e.target.value
+      })
+  }
+  
+  const handleClick = (e) => {
+    e.preventDefault()
+
+    if (!validator.isEmail(signupInput.email)) {
+      return setError("The email you entered is invalid")
+    } else if (signupInput.password.length < 5) {
+      return setError("The password you entered should contain 5 or more characters")
+    } else if (signupInput.password !== signupInput.confirmPassword) {
+      return setError("The passwords don't match")
+    }
+  }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container my-5">
+      <form>
+        <div className="mb-3">
+          <label htmlFor='email' className="form-label">
+            Email address
+          </label>
+          <input 
+          type="email"
+          id='email'
+          name='email'
+          className="form-control"
+          value= {signupInput.email}
+          onChange={handleChange}
+          />
+          
+          <label htmlFor='password' className="form-label">
+            Password
+          </label>
+          <input
+          type="password"
+          id='password'
+          name='password'
+          value={signupInput.password}
+          onChange={handleChange}
+          className="form-control"
+          />
+
+          <label htmlFor='confirm-password' className="form-label">
+            Confirm Password
+          </label>
+          <input
+          type="confirm-password"
+          id='confirm-password'
+          name='confirmPassword'
+          value={signupInput.confirmPassword}
+          onChange={handleChange}
+          className="form-control"
+          />
+
+          {error && <p className="text-danger">{error}</p>}
+
+          <button type='submit' className="btn btn-primary" onClick={handleClick}>Submit</button>
+        </div>
+
+
+      </form>
+     
+          
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,158 @@
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+// jest hooks
+afterEach(() => {
+  console.log("this will run after each test")
 });
+
+beforeAll(() => {
+  console.log("Will run only once before all tests")
+});
+
+afterAll(() => {
+  console.log("This will run once after all of the tests")
+});
+
+// helper functions
+const typeIntoForm = ({email, password, confirmPassword}) => {
+  // these statements find the elements
+  const emailInputElement = screen.getByLabelText("Email address");
+  const passwordInputElement = screen.getByLabelText("Password");
+  const confirmPasswordInputElement = screen.getByLabelText(/confirm password/i)
+
+  // if an email is passed into the function
+  // log a user event
+  if(email){
+    userEvent.type(emailInputElement, email)
+  }
+
+  if (password) {
+    userEvent.type(passwordInputElement, password)
+  }
+
+  if (confirmPassword) {
+    userEvent.type(confirmPasswordInputElement, confirmPassword)
+  }
+
+  return {
+    emailInputElement,
+    passwordInputElement,
+    confirmPasswordInputElement
+  }
+}
+
+const clickSubmitButton = () => {
+  const submitButtonElement = screen.getByRole('button')
+  userEvent.click(submitButtonElement)
+  return submitButtonElement
+}
+
+// Describe blocks
+// the below describe block just says all of these tests are testing the app component
+// describe blocks can be nested
+
+describe("App", () => {
+  beforeEach(() => {
+    render(<App />);
+  });
+  
+  test("inputs should be initially empty", () => {
+    const emailInputElement = screen.getByLabelText("Email address");
+    const passwordInputElement = screen.getByLabelText("Password");
+    const confirmPasswordInputElement = screen.getByLabelText(/confirm password/i)
+    expect(emailInputElement.value).toBe("")
+    expect(passwordInputElement.value).toBe("")
+    expect(confirmPasswordInputElement.value).toBe("")
+  });
+  
+  test("should be able to type in email", () => {
+    // type into form returns the emailInputElement
+    const {emailInputElement} = typeIntoForm({ email: "selena@gmail.com"})
+    expect(emailInputElement.value).toBe("selena@gmail.com")
+  })
+  
+  test("should be able to type a password", () => {
+    const {passwordInputElement} = typeIntoForm({ password: "12345"})
+    expect(passwordInputElement.value).toBe("12345")
+  })
+  
+  test("should be able to confirm a password", () => {
+    const {confirmPasswordInputElement} = typeIntoForm({ confirmPassword: "12345"})
+    expect(confirmPasswordInputElement.value).toBe("12345")
+  })
+
+  // this describe block says the below tests are for error handling
+  describe("Error handling", () => {
+    // this will only run before each of the tests in this block
+    beforeEach(() => {
+      console.log('hello')
+    })
+
+    test("should show email error messgae on invalid email", () => {
+      typeIntoForm({
+        email: "selenagmail.com",
+      })
+    
+      clickSubmitButton()
+    
+      expect (screen.queryByText(/the email you entered is invalid/i)).toBeInTheDocument()
+    })
+    
+    test("password should be 5 or more characters", () => {
+    
+      typeIntoForm({email: 'selena@gmail.com'})
+      
+      expect(screen.queryByText(/the password you entered should contain 5 or more characters/i)).not.toBeInTheDocument()
+    
+      typeIntoForm({password: '123'})
+      
+      clickSubmitButton() 
+    
+      expect(screen.queryByText(/the password you entered should contain 5 or more characters/i)).toBeInTheDocument();
+    })
+    
+    test("passwords should match", () => {
+    
+      typeIntoForm({email: "selena@gmail.com", password: "12345"})
+    
+      expect(screen.queryByText(/the passwords don't match/i)).not.toBeInTheDocument()
+    
+      typeIntoForm({confirmPassword: "123456"})
+      
+      clickSubmitButton()
+    
+      expect(screen.queryByText(/the passwords don't match/i)).toBeInTheDocument();
+    })
+
+    test("should show no error message if every input is valid ", () => {
+  
+      typeIntoForm({
+        email: "selena@gmail.com",
+        password: "12345",
+        confirmPassword: "12345"
+      })
+    
+      clickSubmitButton()
+      // get the error elements
+      // it's not necessary to save these to a variable but leaving this here for clarity
+      const passwordErrorElement = screen.queryByText(/the password you entered should contain 5 or more characters/i)
+      const emailErrorElement = screen.queryByText(/the email you entered is invalid/i)
+      const confirmPasswordErrorElement = screen.queryByText(/the passwords don't match/i)
+    
+      // assert that they are not in the document
+      expect (passwordErrorElement).not.toBeInTheDocument()
+      expect(emailErrorElement).not.toBeInTheDocument()
+      expect(confirmPasswordErrorElement).not.toBeInTheDocument()
+    })
+    
+  })
+  
+  test("submit button should be present", () => {
+  
+    expect(screen.getByRole('button')).toBeInTheDocument()
+  })
+
+})
+
